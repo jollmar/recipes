afbd47139390b08d42468d2b2d76b6a565ffb485
Johan Ljung <jollelj@hotmail.com>
2023-03-12 16:30:52 +0100

    Edit site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Johan och Saras veganska recept";
+export const SITE_TITLE = "Johan & Saras veganska recept";
 export const SITE_DESCRIPTION = "De recept vi ofta lagar";
